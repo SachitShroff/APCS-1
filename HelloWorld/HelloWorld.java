a34b66729588996e09796d0d38f02f3b3e0baff3
SachitShroff <sachit.shroff@gmail.com>
2015-08-18 09:21:29 -0700

    8/18/15 Hello World Demonstration
    I feel OK

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ * Prints Hello World as an introduction to Java
+ * @author Sachit Shroff
+ * @version 1.0 (8/18/15)
+ */
+public class HelloWorld
+{
+    // instance variables - replace the example below with your own
+   public static void main(String[] args)
+   {
+       System.out.println("Hello World");
+    }
+}
---
+++ HelloWorld/HelloWorld2.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class HelloWorld2 here.
+ * Pretty self explanatory
+ * @author Sachit Shroff
+ * @version 1.0 (8/18/15)
+ */
+public class HelloWorld2
+{
+    // instance variables - replace the example below with your own
+   public static void main(String[] args)
+   {
+       System.out.println(getText());
+   }
+   public static String getText()
+   {
+       return "Hello World Round 2!";
+   }
+}
